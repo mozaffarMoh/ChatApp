ed9a6d16718260170caf34898a52557b6f6946bc
Mozaffar <FERAWWWESS@gmail.com>
2024-05-08 19:09:42 +0300

    addIndicator

--- src/components/ChatMessages/ChatMessages.tsx
+++ src/components/ChatMessages/ChatMessages.tsx
@@ -1,5 +1,7 @@
 import { Avatar } from "@mui/material";
 import "./ChatMessages.scss";
+import { IoMdArrowDropright } from "react-icons/io";
+import { IoMdArrowDropleft } from "react-icons/io";
 
 const ChatMessages = () => {
   const array = ["ltr", "ltr", "rtl", "rtl", "ltr"];
@@ -7,7 +9,16 @@ const ChatMessages = () => {
     <div className="chat-messages">
       {array.map((item, index) => {
         return (
-          <div className="message-content-container flexStart" dir={item} key={index}>
+          <div
+            className="message-content-container flexStart"
+            dir={item}
+            key={index}
+          >
+            {item === "rtl" ? (
+              <IoMdArrowDropright className="right-indicator" />
+            ) : (
+              <IoMdArrowDropleft className="left-indicator" />
+            )}
             <Avatar />
             <div className="message-content flexCenter">
               <p>This is test message</p>
