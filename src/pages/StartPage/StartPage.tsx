08b8de20ebf776d175f40ae2771f97bf3c378045
Mozaffar <FERAWWWESS@gmail.com>
2024-05-15 21:03:35 +0300

    edit-styles

--- src/pages/StartPage/StartPage.tsx
+++ src/pages/StartPage/StartPage.tsx
@@ -2,9 +2,29 @@ import { Button } from "@mui/material";
 import "./StartPage.scss";
 import { useNavigate } from "react-router-dom";
 import startImage from "../../assets/images/start-page.png";
+import React from "react";
 
 const StartPage = () => {
   const navigate = useNavigate();
+  let speed = 60;
+  let text =
+    "  Chat helps Workspace users connect and collaborate to get things done.";
+  const [displayedText, setDisplayedText] = React.useState("");
+
+  React.useEffect(() => {
+    if (displayedText.length !== text.length - 1) {
+      let index = 0;
+      const intervalId = setInterval(() => {
+        setDisplayedText((prev) => prev + text.charAt(index));
+        index += 1;
+        if (index >= text.length) {
+          clearInterval(intervalId);
+        }
+      }, speed);
+
+      return () => clearInterval(intervalId);
+    }
+  }, [text]);
   return (
     <div className="start-page flexCenter">
       <div className="action-section flexBetweenColumnItemsStart">
@@ -13,25 +33,22 @@ const StartPage = () => {
           <h2>Messaging and team collaboration</h2>
         </div>
         <div className="description-section">
-          <p>
-            Chat helps Workspace users connect and collaborate to get things
-            done.
-          </p>
+          <p>{displayedText}</p>
         </div>
         <div className="buttons-section flexStart">
+          <Button
+            variant="contained"
+            color="warning"
+            onClick={() => navigate("/sign-up")}
+          >
+            SIGN-UP
+          </Button>
           <Button
             variant="contained"
             color="primary"
             onClick={() => navigate("/login")}
           >
             Login
-          </Button>
-          <Button
-            variant="contained"
-            color="warning"
-            onClick={() => navigate("/sign-up")}
-          >
-            Sign-UP
           </Button>
         </div>
       </div>
