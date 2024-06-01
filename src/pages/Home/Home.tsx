71167688208003423a66c70d0da001cef0476d82
Mozaffar <FERAWWWESS@gmail.com>
2024-06-01 13:43:20 +0300

    fix-layout

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -7,19 +7,19 @@ const Home: React.FC = () => {
   const [isSmallScreen, setIsSmallScreen] = React.useState<boolean>(false);
   const [showUserChat, setShowUserChat] = React.useState<boolean>(false);
 
-
-  React.useEffect(() => {
-    const handleResize = () => {
-      setIsSmallScreen(window.innerWidth <= 700);
-    };
-
+  const handleResize = React.useCallback(() => {
+    setIsSmallScreen(window.innerWidth <= 700);
+  }, []);
+  
+  React.useLayoutEffect(() => {
     handleResize();
+
     window.addEventListener("resize", handleResize);
 
     return () => {
       window.removeEventListener("resize", handleResize);
     };
-  }, []);
+  }, [handleResize]);
 
   return (
     <div className="home flexCenter">
